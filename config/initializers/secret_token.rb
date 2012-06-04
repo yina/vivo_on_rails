93e4a4f7510b67ab52887047aac023c787ad0630
Yin Aphinyanaphongs <yinnerspace@gmail.com>
2012-06-04 14:54:01 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VivoOnRails::Application.config.secret_token = 'ad4729aada89c6cca3a8d4f6e6f18eb0b2d18d89dd527b4443308db3d5a0f1043bf15d9078842576d481ad0973a8d83eb5bed3191f5e4264d092fee73eed22bc'
