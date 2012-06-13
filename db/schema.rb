20c981df098f023e5d1aea256ccc906ab6dd2e2d
Yin Aphinyanaphongs <yinnerspace@gmail.com>
2012-06-13 13:11:55 -0400

    update schema with faculty member table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120606202519) do
+ActiveRecord::Schema.define(:version => 20120613162551) do
 
   create_table "faculty_members", :force => true do |t|
     t.text     "first_name"
