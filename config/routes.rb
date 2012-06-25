28d602890921cf8298e8ba349091a4a53d1788ef
Yin Aphinyanaphongs <yinnerspace@gmail.com>
2012-06-25 17:09:57 -0400

    add home controller and index view from rails generate helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 VivoOnRails::Application.routes.draw do
+  get "home/index"
+
+  get "search/search"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +52,7 @@ VivoOnRails::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
