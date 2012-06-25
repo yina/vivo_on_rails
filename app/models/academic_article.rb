b5bd487f54bd2c518d72dc728bb45e704f1d055b
Yin Aphinyanaphongs <yinnerspace@gmail.com>
2012-06-25 17:02:31 -0400

    index academic articles

--- app/models/academic_article.rb
+++ app/models/academic_article.rb
@@ -5,4 +5,16 @@ class AcademicArticle < ActiveRecord::Base
   attr_accessible :label, :abstract, :issue, :pmid, :datetime
   
   validates :label, :uniqueness => true
+  
+  searchable do
+    text :label, :stored => true
+    text :abstract, :stored => true
+    time :datetime
+    string :author, :multiple => true do
+      faculty_members.map { |faculty_member|
+        out = "#{faculty_member.last_name}, #{faculty_member.first_name}"
+      }
+    end
+  end
+    
 end
